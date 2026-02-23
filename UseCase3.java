32372de88d873cc4b39b51543c4334c4a4707661
j7328-hash <jl7328@srmist.edu.in>
2026-02-23 10:21:59 +0530

    Added uc3

---
+++ UseCase3.java
@@ -0,0 +1,12 @@
+public class UseCase3 {
+    public static void main(String[] args) {
+        // uc2:
+        System.out.println("   ***       *****  ");
+        System.out.println(" **   **    **    * ");
+        System.out.println(" *     *    **      ");
+        System.out.println(" *     *     ***    ");
+        System.out.println(" *     *        **  ");
+        System.out.println(" **   **   *    **  ");
+        System.out.println("   ***      *****   ");
+    }
+}
